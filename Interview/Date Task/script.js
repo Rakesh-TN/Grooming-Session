33f7ea284ab60818ee677c38f2bf7cb083b7e733
Rakesh-TN <tnrakesh68@outlook.com>
2023-10-03 12:02:56 +0530

    JSON

---
+++ Interview/Date Task/script.js
@@ -0,0 +1,10 @@
+let today = new Date()
+alert(today)
+alert(today.getDate()+10)
+
+let alpha = document.querySelector("#notGiftday").value
+let beta = document.querySelector("#calcu").value
+
+function find(){
+    
+}  
